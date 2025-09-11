f664d78b241faf1e4211cbd01e36fedf5c8b5415
T-r-666 <1154730956@qq.com>
2025-09-11 16:13:28 +0800

    kg与pd转化

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # 在这个文件下编写代码，题目具体要求见README.md文件
+TempStr = input("请输入带有符号的重量：")
+if TempStr[-2:] in ['kg']:
+    pd = eval(TempStr[0:-2])*2.2046
+    print("转换后的重量为{:.3f}磅".format(pd))
+elif TempStr[-2:] in ['pd']:
+    kg = eval(TempStr[0:-2])/2.2046    
+    print("转换后的重量为{:.3f}公斤".format(kg))
+else:
+    print("输入格式错误")
