0a9dd172bb00d5251c3559155832392fdef57c11
T-r-666 <1154730956@qq.com>
2025-09-11 16:22:52 +0800

    完成pd与kg之间的转换作业

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # 在这个文件下编写代码，题目具体要求见README.md文件
 TempStr = input("请输入带有符号的重量：")
-if TempStr[-2:] in ['kg']:
-    pd = eval(TempStr[0:-2])*2.2046
-    print("转换后的重量为{:.3f}磅".format(pd))
-elif TempStr[-2:] in ['pd']:
-    kg = eval(TempStr[0:-2])/2.2046    
-    print("转换后的重量为{:.3f}公斤".format(kg))
+unit = TempStr[-2:]
+value = float(TempStr[:-2]) 
+if unit == "kg":
+    pounds = value * 2.2046
+    print(f"对应的英制重量为{pounds:.3f}磅")
+elif unit == "pd":
+    kilograms = value * 0.4535
+    print(f"对应的公制重量为{kilograms:.3f}公斤")
 else:
     print("输入格式错误")
